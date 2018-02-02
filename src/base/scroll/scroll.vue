fce7852ba3b5d07cae0cf4c5316161deb88dc186
e <563798627@qq.com>
2018-02-02 15:20:52 +0800

    add scroll from base components

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="scroll" ref="scrollWrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+
+export default {
+  prop: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    data: {
+      type: Array,
+      default: null
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    refreshDelay: {
+      type: Number,
+      default: 20
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll() {
+      if (!this.$refs.scrollWrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.scrollWrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+    },
+    disable() {
+      this.scroll && this.scroll.disable()
+    },
+    enable() {
+      this.scroll && this.scroll.enable()
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh()
+    }
+  },
+  watch: {
+    data() {
+      setTimeout(() => {
+        this.refresh()
+      }, this.refreshDelay)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,33 +1,40 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div class="slider-wrapper" ref="sliderWrapper" v-if="recommends.length">
-      <slide>
-        <div v-for="item in recommends" :key="item.code">
-          <a :href="item.linkUrl">
-            <img :src="item.picUrl">
-          </a>
+    <!-- discList的作用是监听数据，重新刷新Scroll组件 -->
+    <scroll class="recommend-content" :data="discList">
+      <div>
+        <div class="slider-wrapper" ref="sliderWrapper" v-if="recommends.length">
+          <slide>
+            <div v-for="item in recommends" :key="item.code">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl">
+              </a>
+            </div>
+          </slide>
         </div>
-      </slide>
-    </div>
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
-      <ul>
-        <li v-for="item in discList" :key="item.code" class="item">
-          <div class="icon">
-            <img :src="item.imgurl" width="60" height="60">
-          </div>
-          <div class="text">
-            <h2 class="name" v-text="item.creator.name"></h2>
-            <p class="desc" v-text="item.dissname"></p>
-          </div>
-        </li>
-      </ul>
-    </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" :key="item.code" class="item">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60">
+              </div>
+              <div class="text">
+                <h2 class="name" v-text="item.creator.name"></h2>
+                <p class="desc" v-text="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Slide from 'base/slide/slide'
+import Scroll from 'base/scroll/scroll'
+import {ERR_OK} from '@/api/config'
 import {getRecommend, getDiscList} from '@/api/recommends'
 export default {
   data() {
@@ -37,24 +44,30 @@ export default {
     }
   },
   created() {
-    this._getRecommends()
-    this._getDiscList()
+    setTimeout(() => {
+      this._getRecommends()
+      this._getDiscList()
+    }, 20)
   },
   methods: {
     _getRecommends() {
       getRecommend().then((res) => {
-        this.recommends = res.data.slider
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
       })
     },
     _getDiscList() {
       getDiscList().then((res) => {
-        console.log(res)
-        this.discList = res.data.list
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
+        }
       })
     }
   },
   components: {
-    Slide
+    Slide,
+    Scroll
   }
 }
 </script>
@@ -67,37 +80,40 @@ export default {
     width: 100%
     top: 88px
     bottom: 0
-    .slider-wrapper
-      position: relative
-      width: 100%
+    .recommend-content
+      height: 100%
       overflow: hidden
-    .recommend-list
-      .list-title
-        height: 65px
-        line-height: 65px
-        text-align: center
-        font-size: $font-size-medium
-        color: $color-theme
-      .item
-        display: flex
-        box-sizing: border-box
-        align-items: center
-        padding: 0 20px 20px 20px
-        .icon
-          flex: 0 0 60px
-          padding-right: 20px
-          width: 60px
-        .text
-          display: flex
-          flex-direction: column
-          justify-content: center
-          flex: 1
-          line-height: 20px
-          overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
           font-size: $font-size-medium
-          .name
-            margin-bottom: 10px
-            color: #ffffff
-          .desc
-            color: $color-text-d
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            padding-right: 20px
+            width: 60px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: #ffffff
+            .desc
+              color: $color-text-d
 </style>
